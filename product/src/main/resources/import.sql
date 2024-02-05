801f08c4d229d2c2f0e0f01e9c0810869f330206
manueladriancaceres <adriancaceres980@gmail.com>
2024-02-05 13:39:00 -0300

    change origin curse

--- product/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-insert into products (name,price,create_at) values('Panasonic',800,NOW());
-insert into products (name,price,create_at) values('Longvie',900,NOW());
-insert into products (name,price,create_at) values('Patrick',10000,NOW());
